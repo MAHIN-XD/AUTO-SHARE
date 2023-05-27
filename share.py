53a90345ffc865b6533ff2d1114689b75525d2c2
Mahin Bro <117618133+MAHIN-XD@users.noreply.github.com>
2023-05-28 01:40:39 +0600

    Add files via upload

--- share.py
+++ share.py
@@ -4,15 +4,11 @@ try:
  import requests
 except:os.system("pip uninstall requests -y;pip install requests")
  
-print('\033[1;91m[\033[1;97m-\033[1;91m] \033[1;97mChecking For Update...')
+
 os.system('git pull --quiet 2>/dev/null')
 bit = platform.architecture()[0]
 if bit == '64bit':
- print('\033[1;93m[\033[1;92m•\033[1;93m] \033[1;97mChecking For Update ')
- print('\033[1;93m[\033[1;92m•\033[1;93m] \033[1;97mAll Done')
  import SHARE
 elif bit == '32bit':
- print('\033[1;93m[\033[1;92m•\033[1;93m] \033[1;97mChecking For Update ')
- print('\033[1;93m[\033[1;92m•\033[1;93m] \033[1;97mAll Done')
  import SHARE_32
  
