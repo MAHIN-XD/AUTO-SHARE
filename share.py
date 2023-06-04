c443b53d79def287d535c01bfaa2b4628a15625a
Mahin Bro <117618133+MAHIN-XD@users.noreply.github.com>
2023-06-04 17:29:27 +0600

    Update share.py

--- share.py
+++ share.py
@@ -5,6 +5,7 @@ try:
 except:os.system("pip uninstall requests -y;pip install requests")
  
 
+os.system('xdg-open https://youtube.com/@mrmahin248')
 os.system('git pull')
 bit = platform.architecture()[0]
 if bit == '64bit':
